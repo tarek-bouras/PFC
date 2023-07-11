e0ac6a98dbde821161799a6d7b9121dd26c4af8d
Tarek Bouras <82026951+tarek-bouras@users.noreply.github.com>
2023-07-11 19:21:02 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+237ij2u=n#c^8vhu2$8qsa5)-db$(%7_zm&-$k$q=6d2x_-w2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
